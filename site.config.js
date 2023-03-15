d85d2f519c14aa476d8df18c804082506bcb3b67
Ashfid <ashfidable.islam@gmail.com>
2023-03-16 01:37:54 +0300

    feat: contact js file added

---
+++ site.config.js
@@ -0,0 +1,22 @@
+export const contact = [
+    {
+        "name": "Discord",
+        "value": "K0СТУР#2004",
+        "icon": "ic:baseline-discord"
+    },
+    {
+        "name": "Email",
+        "value": "xyz@gmail.com",
+        "icon": "ic:baseline-mail"
+    },
+    {
+        "name": "Bio",
+        "value": "I create things.",
+        "icon": "ic:baseline-outlet"
+    },
+    {
+        "name": "Location",
+        "value": "Sweden",
+        "icon": "ic:baseline-location-on"
+    },
+]
