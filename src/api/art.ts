09618970fdf870b549c298d28b420b07113334df
Ashfid <ashfidable.islam@gmail.com>
2023-03-21 07:33:10 +0300

    feat:  typed apis for games and art (zod)

---
+++ src/api/art.ts
@@ -0,0 +1,43 @@
+import { z } from 'zod'
+
+const ArtworksSchema = z.array(
+    z.object({
+        title: z.string(),
+        id: z.number(),
+        default: z.object({
+            src: z.string(),
+            width: z.number(),
+            height: z.number(),
+            format: z.string(),
+        }),
+    }),
+)
+
+const Artwork = ArtworksSchema.element;
+
+export type Artworks = z.infer<typeof ArtworksSchema>
+export type Artwork = z.infer<typeof Artwork>
+
+export default function getAllArt(artworks: Artworks): Artworks {
+
+    return artworks = artworks.map(art => {
+        const src = art.default.src;
+        const regex = import.meta.env.PROD
+            ? /\/(\d+)-([^\/]+)\.\w+$/
+            : /\/(\d+)-(.+)\.png\?/;
+        const match = src.match(regex);
+        let id = 0;
+        let title = "";
+        if (match) {
+            id = Number(match[1])
+            title = match[2]
+        }
+
+        return {
+            ...art,
+            id,
+            title
+        }
+
+    })
+}
--- src/api/games.ts
+++ src/api/games.ts
@@ -1,6 +1,26 @@
+import { z } from 'zod';
+
+const GamesSchema = z.array(
+    z.object({
+        title: z.string(),
+        url: z.string().url(),
+        cover_url: z.string().optional(),
+    })
+)
+
+export type Games = z.infer<typeof GamesSchema>
+
+
 export default async function getAllGames() {
     const response = await fetch('https://itch.io/api/1/iCQzGljw8jsSCmjYNewsqHDS7ew1ukuXiCOmaXqg/my-games')
     const result = await response.json();
 
-    return result["games"];
+    if (GamesSchema.safeParse(result["games"]).success) {
+        console.log("Itch.io games are successfully retrieved and schema is matched")
+        return result["games"];
+    }
+    else {
+        console.log("Itch.io API failed to get all the games and it doesn't match the schema.")
+        return [];
+    }
 }
