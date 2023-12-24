7559f4c221f5197e1032a6248578c6ffe8dedf95
Ashfid <ashfidable.islam@gmail.com>
2023-12-25 00:55:40 +0300

    fix: theme toggler local storage support

--- src/components/theme-toggler.svelte
+++ src/components/theme-toggler.svelte
@@ -1,49 +1,51 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-    import extractThemes from "../scripts/extractThemes";
+  import { onMount } from "svelte";
+  import extractThemes from "../scripts/extractThemes";
 
-    let html: HTMLElement;
+  let html: HTMLElement;
 
-    let themes = extractThemes();
+  let themes = extractThemes();
 
-    let selectedTheme: string | null =
-        localStorage.getItem("theme") !== null
-            ? localStorage.getItem("theme")
-            : "default";
+  let selectedTheme: string | null = null
 
     $: {
-        if (html) {
-            localStorage.setItem("theme", selectedTheme ?? "default");
-            html.dataset.theme = selectedTheme ?? "default";
-        }
+      if (html) {
+        localStorage.setItem("theme", selectedTheme ?? "default");
+        html.dataset.theme = selectedTheme ?? "default";
+      }
     }
 
-    onMount(() => {
-        html = document.documentElement;
-    });
+  onMount(() => {
+    html = document.documentElement;
+
+    selectedTheme = localStorage.getItem("theme") !== null && typeof window !== "undefined"
+        ? localStorage.getItem("theme")
+        : "default";
+    
+  });
 </script>
 
 <form class="flex flex-wrap items-center justify-center gap-2">
-    {#each themes as theme}
-        <div data-theme={theme}>
-            <input
-                type="radio"
-                data-theme={theme}
-                id={theme}
-                name={theme}
-                value={theme}
-                bind:group={selectedTheme}
-            />
-            <label
-                class="flex overflow-hidden border-2 rounded-full cursor-pointer border-primary hover:animate-gelatine-in-out"
-                for={theme}
-            >
-                <div class="w-6 h-16 lg:w-2 lg:h-6 bg-primary" />
-                <div class="w-6 h-16 lg:w-2 lg:h-6 bg-secondary" />
-                <div class="w-6 h-16 lg:w-2 lg:h-6 bg-color" />
-            </label>
-        </div>
-    {/each}
+  {#each themes as theme}
+    <div data-theme={theme}>
+      <input
+        type="radio"
+        data-theme={theme}
+        id={theme}
+        name={theme}
+        value={theme}
+        bind:group={selectedTheme}
+      />
+      <label
+        class="flex overflow-hidden border-2 rounded-full cursor-pointer border-primary hover:animate-gelatine-in-out"
+        for={theme}
+      >
+        <div class="w-6 h-16 lg:w-2 lg:h-6 bg-primary" />
+        <div class="w-6 h-16 lg:w-2 lg:h-6 bg-secondary" />
+        <div class="w-6 h-16 lg:w-2 lg:h-6 bg-color" />
+      </label>
+    </div>
+  {/each}
 </form>
 
 <!-- <script>
@@ -59,9 +61,9 @@
     });
 </script> -->
 <style>
-    input[type="radio"] {
-        position: fixed;
-        opacity: 0;
-        pointer-events: none;
-    }
+  input[type="radio"] {
+    position: fixed;
+    opacity: 0;
+    pointer-events: none;
+  }
 </style>
