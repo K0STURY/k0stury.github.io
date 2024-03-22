3c3842945ae54778c89aa0b892c890c3e240770f
Ashfid <ashfidable.islam@gmail.com>
2024-03-23 02:48:51 +0300

    feat: new globals variable in site.config.js

--- site.config.js
+++ site.config.js
@@ -1,18 +1,22 @@
 export const contact = [
-    {
-        "name": "Discord",
-        "value": "K0STUR",
-        "icon": "ic:baseline-discord",
-        "action": "copy"
-    },
-    {
-        "name": "Bio",
-        "value": "I am a hobbiest artist from Sweden, programming is my passion",
-        "icon": "ic:baseline-outlet"
-    },
-    {
-        "name": "Location",
-        "value": "Sweden",
-        "icon": "ic:baseline-location-on"
-    },
-]
+  {
+    name: "Discord",
+    value: "K0STUR",
+    icon: "ic:baseline-discord",
+    action: "copy",
+  },
+  {
+    name: "Bio",
+    value: "I am a hobbiest artist from Sweden, programming is my passion",
+    icon: "ic:baseline-outlet",
+  },
+  {
+    name: "Location",
+    value: "Sweden",
+    icon: "ic:baseline-location-on",
+  },
+];
+
+export const globals = {
+  imageQuality: "high", // low, mid, high or max
+};
