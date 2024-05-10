aaecbb94a2a842bb7ca4704679beec70e5a33543
Ashfid <ashfidable.islam@gmail.com>
2024-05-10 08:54:34 +0300

    feat: new accent color and font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,22 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'primary': 'var(--primary-color)',
-				'secondary': 'var(--secondary-color)',
-				'color': 'var(--text-color)'
-			},
-			animation: {
-				'gelatine-infite': 'gelatine 0.375s infinite',
-				'gelatine-in-out': 'gelatine 0.375s ease-in-out'
-			},
-			fontFamily: {
-				'content': ['inter'],
-				'stylized': ['Rancho', 'cursive'],
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--primary-color)",
+        secondary: "var(--secondary-color)",
+        color: "var(--text-color)",
+        accent: "var(--accent-color)",
+      },
+      animation: {
+        "gelatine-infite": "gelatine 0.375s infinite",
+        "gelatine-in-out": "gelatine 0.375s ease-in-out",
+      },
+      fontFamily: {
+        accent: "var(--font-accent)",
+        content: ["inter"],
+        stylized: ["Rancho", "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
